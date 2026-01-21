613c6c04ecd58085e823912444e9ecd3fd6c6eb5
eldar <eldaryergengali@gmail.com>
2026-01-21 21:50:03 +0500

    init: WebFlux product service

---
+++ src/main/java/kz/eldar/product_service/productserviceflux/ProductservicefluxApplication.java
@@ -0,0 +1,13 @@
+package kz.eldar.product_service.productserviceflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductservicefluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductservicefluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/eldar/product_service/productserviceflux/ProductservicefluxApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.eldar.product_service.productserviceflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductservicefluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
