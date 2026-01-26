bf0e86209b57377bec04ad3878fe46edd044a07f
eldar <eldaryergengali@gmail.com>
2026-01-26 19:47:02 +0500

    Homework 10 done

--- src/main/java/kz/eldar/product_service/productserviceflux/config/SecurityConfig.java
+++ src/main/java/kz/eldar/product_service/productserviceflux/config/SecurityConfig.java
@@ -29,7 +29,7 @@ import java.util.List;
 public class SecurityConfig {
 
     private final SecurityProperties securityProperties;
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper ;
 
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
